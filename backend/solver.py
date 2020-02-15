141f7e41d927ac89940cc58cc869b6106b54f536
Andrii Litvynchuk <acethecreator111@gmail.com>
2020-02-15 21:39:06 +0200

    Add solver with situation classification

--- backend/solver.py
+++ backend/solver.py
@@ -1,12 +1,14 @@
 import os
-from typing import NoReturn, Tuple, Union
+from typing import NoReturn, Tuple, Union, List
 
 from matplotlib import pyplot as plt
 import numpy as np
 
 
 class Solver:
-    def __init__(self, input_folder: str) -> NoReturn:
+    def __init__(
+        self, input_folder: str, classification_thresholds: Tuple[float] = (0.3, 0.7)
+    ) -> NoReturn:
         self.a_hat: np.ndarray = np.loadtxt(os.path.join(input_folder, "a.txt"))
         self.i_d_hat: np.ndarray = np.loadtxt(os.path.join(input_folder, "i_d.txt"))
         self.i_p_hat: np.ndarray = np.loadtxt(os.path.join(input_folder, "i_p.txt"))
@@ -15,6 +17,12 @@ class Solver:
         self.alpha: np.ndarray = np.empty(0)
         self.beta: np.ndarray = np.empty(0)
         self.gamma: np.ndarray = np.empty(0)
+        self.t_0: List[List[List[float], str]] = [
+            [[0, 0] for _ in range(self.a_hat.shape[1])]
+            for _ in range(self.a_hat.shape[0])
+        ]
+        self.classification_thresholds: Tuple[float] = classification_thresholds
+        self.situation_classification: List[str] = []
 
     def count_alpha(self) -> np.ndarray:
         alpha: np.ndarray = np.zeros(self.a_hat.shape)
@@ -70,16 +78,54 @@ class Solver:
     def i(self, t: float) -> np.ndarray:
         return self.i_t(t) * self.i_p(t) * self.i_d(t)
 
-    def solve(self):
+    def eta(self, t: float) -> np.ndarray:
+        output: np.ndarray = np.zeros(self.a_hat.shape)
+        output[self.mask] = 1 - np.log(
+            1 + (self.alpha[self.mask] + 220) * self.i(t)[self.mask]
+        )
+        return output
+
+    def solve(
+        self, eta_max: float = 0.9, t_max: int = 20
+    ) -> Tuple[List[List[List[float], str]], List[str]]:
         self.alpha: np.ndarray = self.count_alpha()
         self.beta: np.ndarray = self.count_beta()
         self.gamma: np.ndarray = self.count_gamma()
+
+        for s_index in range(self.a_hat.shape[0]):
+            t_0_min = np.inf
+            for f_index in range(self.a_hat.shape[1]):
+                if self.a_hat[s_index, f_index] != 0:
+                    tmp_t_range = []
+                    for t_index in np.arange(0, 20, 0.1):
+                        if 0 < self.eta(t_index)[s_index][f_index] < eta_max:
+                            tmp_t_range.append(t_index)
+                    if len(tmp_t_range) > 0:
+                        self.t_0[s_index][f_index] = (
+                            min(tmp_t_range),
+                            max(tmp_t_range),
+                        )
+                        t_0_min = min(t_0_min, max(tmp_t_range))
+                    else:
+                        self.t_0[s_index][f_index] = "Not exist"
+                        t_0_min = 0
+                else:
+                    self.t_0[s_index][f_index] = "Not available"
+            if t_0_min < self.classification_thresholds[0]:
+                self.situation_classification.append("Особливо небезпечна ситуація")
+            elif t_0_min < self.classification_thresholds[1]:
+                self.situation_classification.append(
+                    "Клас потенційно небезпечних ситуацій"
+                )
+            else:
+                self.situation_classification.append("Клас майже безпечних ситуацій")
+        return self.t_0, self.situation_classification
 
     def plot_i(
         self,
         s: int = 0,
         f: int = 0,
-        t_range_edges: Tuple[Union[int, float]] = (0, 7),
+        t_range_edges: Tuple[Union[int, float]] = (0, 50),
         delta: float = 0.01,
     ) -> NoReturn:
         t_min, t_max = t_range_edges[0], t_range_edges[1]
@@ -89,9 +135,9 @@ class Solver:
         i_p_range: np.ndarray = np.array([self.i_p(t)[s, f] for t in t_range])
         fig, axis = plt.subplots(3, sharey=True, figsize=(12, 8))
         fig.suptitle("Зміна показників інформованості у процесі формування рішення")
-        axis[0].plot(t_range, i_d_range)
-        axis[1].plot(t_range, i_p_range)
-        axis[2].plot(t_range, i_t_range)
+        axis[0].plot(t_range[i_d_range < 1], i_d_range[i_d_range < 1])
+        axis[1].plot(t_range[i_p_range < 1], i_p_range[i_p_range < 1])
+        axis[2].plot(t_range[i_t_range > 0], i_t_range[i_t_range > 0])
         axis[0].set_title("І_Д")
         axis[1].set_title("І_П")
         axis[2].set_title("І_Т")
